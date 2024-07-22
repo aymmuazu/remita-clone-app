68038ec69318c9df22bb2ba0dad9c7a230c54f6a
Abdurrahim Yahya Muazu <aymmuazu@gmail.com>
2024-07-22 10:32:12 -0400

    New Commit

--- response.php
+++ response.php
@@ -1,32 +1,29 @@
 <?php
-if (isset($_GET['RRR']) && isset($_GET['orderID']) ) {
-    $rrr = $_GET['RRR'];
-    $orderId = $_GET['orderID'];
-    $merchantId = 2547916;
-    $apiKey = 1946;
-    $apiHash = hash('sha512', $rrr.''.$apiKey.''.$merchantId);
+    if (isset($_GET['RRR']) && isset($_GET['orderID']) ) {
+        $rrr = $_GET['RRR'];
+        $orderId = $_GET['orderID'];
+        $merchantId = 2547916;
+        $apiKey = 1946;
+        $apiHash = hash('sha512', $rrr.''.$apiKey.''.$merchantId);
 
-    $curl = curl_init();
+        $curl = curl_init();
 
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => 'http://www.remitademo.net/remita/ecomm/'.$merchantId.'/'.$rrr.'/'.$apiHash.'/status.reg',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json',
-        'Authorization: remitaConsumerKey='.$merchantId.',remitaConsumerToken='.$apiHash.''
-    ),
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://demo.remita.net/remita/exapp/api/v1/send/api/echannelsvc/'.$merchantId.'/'.$rrr.'/'.$apiHash.'/status.reg',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: remitaConsumerKey='.$merchantId.',remitaConsumerToken='.$apiHash.''
+        ),
     ));
-
     $response = curl_exec($curl);
-
     curl_close($curl);
-
     $obj = json_decode($response, true);
 
     $status = $obj['status'];
@@ -41,30 +38,18 @@ if (isset($_GET['RRR']) && isset($_GET['orderID']) ) {
     elseif($status == '023'){
         echo '<script>window.location.href="pay.php";</script>';
     }
-    elseif($status == '01'){
-        
+    elseif($status == '00'){
     }
     else{
         echo '<script>window.location.href="pay.php";</script>';
     }
 ?>
 
-<title>Proceed to Pay  - Remita</title>
-<link rel="stylesheet" href="assets/bootstrap.css">
-<link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-<style>
-    body{
-        font-family: 'Poppins', sans-serif;
-    }
-</style>
-<div class="container pt-5">
-    <div class="text-center">
-        <img src="assets/logo.png" alt="" style="width: 10%;">
-    </div>
-    
-    <h2 class="pt-5 text-center">All About It's PHP Programming Language API</h2>
-    <hr>
+<title>Callback - Remita-Clone App</title>
+<?php include('header.php') ?> 
 
+<div class="container pt-0">
+    
     <div class="alert alert-success text-center">
         Congratulations! You just make a payment
     </div>
@@ -93,11 +78,12 @@ if (isset($_GET['RRR']) && isset($_GET['orderID']) ) {
     </table>
 
     <p class="pt-5 text-center">
-        <a onclick="print()" class="btn btn-primary">Print Page</a>
+        <a onclick="print()" class="btn btn-danger">Print Page</a>
         <a onclick="window.location.href='index.php'" class="btn btn-primary">Home</a>
     </p>
-
 </div>
+
+<?php include('footer.php') ?> 
 
 <?php
 }else{
